08d5e47cb98485ff0ce6d91dc64a50e161e179f5
yuelinwen <tyleryuelinwen@gmail.com>
2021-12-24 08:58:22 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.componenet';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>)
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
