c9dedc710a17586b163aca596f71a14462d715da
yuelinwen <tyleryuelinwen@gmail.com>
2021-12-24 19:11:40 -0500

    sign in page, shop page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.componenet';
+
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>)
-}
+import ShopPage from './pages/shop/shop.componenet';
+import HomePage from './pages/homepage/homepage.componenet';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.componenet';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/sign-in' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
