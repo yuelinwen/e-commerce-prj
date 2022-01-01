6630456594591fa67e3ba04478e27b99514cc0fb
yuelinwen <tyleryuelinwen@gmail.com>
2022-01-01 11:43:42 -0500

    redux-persist feature

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store'
+import { store, persistor } from './redux/store';
+
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}> 
+      {/* use redux persist to store data into browswer, ex: the shopping cart item still there after refresh */}
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
