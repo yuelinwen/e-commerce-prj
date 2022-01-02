1e84be8ea43edf17b74d23e2d201701638b6a142
yuelinwen <tyleryuelinwen@gmail.com>
2022-01-02 18:22:37 -0500

    add stripe payment feature, fix bugs

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import {
   selectCartItems,
@@ -35,8 +36,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
 
     <div className="total">
-      <span>Total: ${total}</span>
+      <div>Total: ${total}</div>
+      <div className="test-warning">
+        * Please use the following fake credit card for testing payment *
+        <br />
+        4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+      </div>
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import rootReducer from "./root-reducer";
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
@@ -9,4 +10,4 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+export default { store, persistStore };
