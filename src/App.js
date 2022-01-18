da370e6585592c33e4f6d351a84bd2600284cdc0
yuelinwen <tyleryuelinwen@gmail.com>
2022-01-18 10:06:32 -0500

    mod

--- src/App.js
+++ src/App.js
@@ -29,8 +29,10 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props
+    //additional
     const { fetchCollectionsStart } = this.props;
     fetchCollectionsStart();
+    //*********** */
     //const { setCurrentUser, collectionsArray } = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -69,6 +71,7 @@ const mapStateToProps = createStructuredSelector({
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
+  //additional
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 })
 
